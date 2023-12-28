fff6b391f41bf87f17f41b157ea926f67d454254
김동찬 <129833370+dongchandev@users.noreply.github.com>
2023-12-28 23:49:29 +0900

    feat: entity작성 완료

--- src/main/java/com/b1nd/alimoserver/domain/vote/adapter/out/persistence/entity/OptionJpaEntity.java
+++ src/main/java/com/b1nd/alimoserver/domain/vote/adapter/out/persistence/entity/OptionJpaEntity.java
@@ -2,10 +2,12 @@ package com.b1nd.alimoserver.domain.option.adapter.out.persistence.entity;
 
 import com.b1nd.alimoserver.global.lib.jpa.BaseTimeEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicUpdate;
 
 @Entity
@@ -14,12 +16,14 @@ import org.hibernate.annotations.DynamicUpdate;
 @DynamicUpdate
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class NotificationJpaEntity extends BaseTimeEntity {
+public class OptionJpaEntity extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long notificationId;
+    private Long optionId;
 
-    @Column(columnDefinition = "TEXT")
+    @Size(max = 200)
     private String content;
+
+    private Long count = 0L;
 }
